1ac1f56e5514adc4daca622a21a67cb30e9b3b57
Spalynx <Spalynx@gmail.com>
2016-02-11 18:37:46 -0600

    Once more, with feeling.

---
+++ src/com/Spal/Main.java
@@ -0,0 +1,17 @@
+package com.Spal;
+import java.util.Scanner;
+/** Going off of memory, this program pretty much splits the words
+ *  of a corpus, rids all non-uniques, delimiters, and stopwords.
+ *
+ *  @author Mark Eikel
+ *  @author Brandon Hathaway
+ *  @version 0.0.1
+ *  @since 0.0.1
+ */
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+    }
+}
