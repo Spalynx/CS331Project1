9d0306f69c4bf2c0053b41c3589f74355235e7bb
Spalynx <Spalynx@gmail.com>
2016-02-13 01:39:18 -0600

    Doesn't work, doesn't matter!

--- src/com/Spal/Main.java
+++ src/com/Spal/Main.java
@@ -1,5 +1,9 @@
 package com.Spal;
+import java.io.*;
+import java.util.ArrayList;
 import java.util.Scanner;
+import java.util.regex.Pattern;
+
 /** Going off of memory, this program pretty much splits the words
  *  of a corpus, rids all non-uniques, delimiters, and stopwords.
  *
@@ -10,8 +14,29 @@ import java.util.Scanner;
  */
 
 public class Main {
-
-    public static void main(String[] args) {
+    public static void main(String[] args) throws FileNotFoundException {
 	// write your code here
+        Scanner scan = new Scanner(new FileReader("src\\com\\Spal\\alice29.txt"));
+        String yes;
+        for(int i =0; i < 20; i++){
+            yes = scan.next();
+            System.out.println(yes);
+        }
+
+    }
+
+    public static void preprocessorTokenization(File toread, File towrite){
+        try {
+            FileWriter writer = new FileWriter(towrite);
+            Scanner reader = new Scanner(toread);
+            ArrayList<String> linewords = new ArrayList<>();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        //reader.useDelimeter(".");
+
+
     }
 }
